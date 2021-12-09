8e1b158dab4edd3519e486caadef0791dcde9187
Gordon Watts <gwatts@uw.edu>
2021-12-09 20:30:29 +0100

    Add support for python 3.10

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name="servicex_clients",
       url="https://github.com/ssl-hep/servicex_clients",
       license="TBD",
       install_requires=[
-          "func_adl_servicex==1.1.2",
+          "func_adl_servicex==1.1.3",
           "tcut-to-qastle==0.5",
       ],
       classifiers=[
